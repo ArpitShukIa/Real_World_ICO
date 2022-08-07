89dc1471bceb3d702db3fc950f06dc44c4d91a01
Arpit Shukla <arpitshukla62@gmail.com>
2022-08-07 20:40:21 +0530

    Setup token distribution

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -7,22 +7,35 @@ CAP = ether(100)
 GOAL = ether(50)
 
 WEEK_IN_SECONDS = 7 * 24 * 60 * 60  # Seconds in 1 week
+YEAR_IN_SECONDS = 365 * 24 * 60 * 60  # Seconds in 1 year
 
 
-def deploy():
+def deploy_dapp_token():
     account = get_account()
+    dapp_token = DappToken.deploy('Dapp Token', 'DAPP', 18, {'from': account})
+    return dapp_token
+
+
+def deploy_dapp_token_crowdsale(dapp_token):
+    owner = get_account(index=0)
     wallet = get_account(index=1)
+    founders_fund = get_account(index=4)
+    foundation_fund = get_account(index=5)
+    partners_fund = get_account(index=6)
+
     opening_time = chain.time() + WEEK_IN_SECONDS
     closing_time = opening_time + WEEK_IN_SECONDS
-    dapp_token = DappToken.deploy('Dapp Token', 'DAPP', 18, {'from': account})
+    release_time = closing_time + YEAR_IN_SECONDS
     dapp_token_crowdsale = DappTokenCrowdsale.deploy(
         CROWDSALE_RATE, wallet, dapp_token, CAP, opening_time, closing_time, GOAL,
-        {'from': account}
+        founders_fund, foundation_fund, partners_fund, release_time,
+        {'from': owner}
     )
     dapp_token.pause()
     dapp_token.transferOwnership(dapp_token_crowdsale)
-    return dapp_token, dapp_token_crowdsale
+    return dapp_token_crowdsale
 
 
 def main():
-    deploy()
+    dapp_token = deploy_dapp_token()
+    deploy_dapp_token_crowdsale(dapp_token)
--- tests/test_crowdsale.py
+++ tests/test_crowdsale.py
@@ -1,8 +1,9 @@
 import pytest
-from brownie import exceptions, chain, web3, project, config, DappToken, Contract
+from brownie import exceptions, chain, project, config
 from web3 import Web3
 
-from scripts.deploy import deploy, CROWDSALE_RATE, WEEK_IN_SECONDS
+from scripts.deploy import deploy_dapp_token, deploy_dapp_token_crowdsale, \
+    CROWDSALE_RATE, WEEK_IN_SECONDS, YEAR_IN_SECONDS
 from scripts.helpful_scripts import get_account, ether
 
 INVESTOR_MIN_CAP = ether(0.002)
@@ -12,22 +13,23 @@ ICO_STAGE = 1
 PRE_ICO_RATE = 500
 ICO_RATE = 250
 
-RefundVault = project.load(config["dependencies"][0]).RefundVault
+openzeppelin = project.load(config["dependencies"][0])
+RefundVault = openzeppelin.RefundVault
+TokenTimelock = openzeppelin.TokenTimelock
 
 
 @pytest.fixture
-def dapp_token_crowdsale():
+def dapp_token():
+    return deploy_dapp_token()
+
+
+@pytest.fixture
+def dapp_token_crowdsale(dapp_token):
     investor1, investor2 = get_account(index=2), get_account(index=3)
-    _, dapp_token_crowdsale = deploy()
+    dapp_token_crowdsale = deploy_dapp_token_crowdsale(dapp_token)
     chain.sleep(WEEK_IN_SECONDS)
     dapp_token_crowdsale.addManyToWhitelist([investor1, investor2])
     return dapp_token_crowdsale
-
-
-@pytest.fixture
-def dapp_token(dapp_token_crowdsale):
-    dapp_token_address = dapp_token_crowdsale.token()
-    return Contract.from_abi('DappToken', dapp_token_address, DappToken.abi)
 
 
 def test_contracts_deployed_correctly(dapp_token, dapp_token_crowdsale):
@@ -138,8 +140,12 @@ def test_finalizing_crowdsale_when_goal_not_reached(dapp_token_crowdsale):
 
 def test_finalizing_crowdsale_when_goal_reached(dapp_token, dapp_token_crowdsale):
     owner = get_account()
+    wallet = get_account(index=1)
     investor1 = get_account(index=2)
     investor2 = get_account(index=3)
+    founders_fund = get_account(index=4)
+    foundation_fund = get_account(index=5)
+    partners_fund = get_account(index=6)
     refund_vault_address = dapp_token_crowdsale.vault()
     refund_vault = RefundVault.at(refund_vault_address)
 
@@ -154,9 +160,78 @@ def test_finalizing_crowdsale_when_goal_reached(dapp_token, dapp_token_crowdsale
     assert dapp_token.mintingFinished()
     assert not dapp_token.paused()
 
+    # Transfers ownership to the wallet
+    assert dapp_token.owner() == wallet
+
     # Verify that token transfer is now allowed
     dapp_token.transfer(investor2, 1, {'from': investor1})
 
     # Verify that claiming refund is not allowed
     with pytest.raises(exceptions.VirtualMachineError):
         refund_vault.refund(investor1, {'from': investor1})
+
+    total_supply = dapp_token.totalSupply()
+    decimals = dapp_token.decimals()
+
+    # Founders
+    founders_timelock_address = dapp_token_crowdsale.foundersTimelock()
+    founders_timelock_balance = dapp_token.balanceOf(founders_timelock_address)
+    founders_timelock_balance = founders_timelock_balance // (10 ** decimals)
+
+    founders_percentage = dapp_token_crowdsale.foundersPercentage()
+    founders_amount = total_supply * founders_percentage / 100
+    founders_amount = founders_amount // (10 ** decimals)
+
+    assert founders_amount == founders_timelock_balance
+
+    # Can't withdraw from time lock
+    founders_timelock = TokenTimelock.at(founders_timelock_address)
+    with pytest.raises(exceptions.VirtualMachineError):
+        founders_timelock.release({'from': owner})
+
+    # Foundation
+    foundation_timelock_address = dapp_token_crowdsale.foundationTimelock()
+    foundation_timelock_balance = dapp_token.balanceOf(foundation_timelock_address)
+    foundation_timelock_balance = foundation_timelock_balance // (10 ** decimals)
+
+    foundation_percentage = dapp_token_crowdsale.foundationPercentage()
+    foundation_amount = total_supply * foundation_percentage / 100
+    foundation_amount = foundation_amount // (10 ** decimals)
+
+    assert foundation_amount == foundation_timelock_balance
+
+    # Can't withdraw from time lock
+    foundation_timelock = TokenTimelock.at(foundation_timelock_address)
+    with pytest.raises(exceptions.VirtualMachineError):
+        foundation_timelock.release({'from': owner})
+
+    # Partners
+    partners_timelock_address = dapp_token_crowdsale.partnersTimelock()
+    partners_timelock_balance = dapp_token.balanceOf(partners_timelock_address)
+    partners_timelock_balance = partners_timelock_balance // (10 ** decimals)
+
+    partners_percentage = dapp_token_crowdsale.partnersPercentage()
+    partners_amount = total_supply * partners_percentage / 100
+    partners_amount = partners_amount // (10 ** decimals)
+
+    assert partners_amount == partners_timelock_balance
+
+    # Can't withdraw from time lock
+    partners_timelock = TokenTimelock.at(partners_timelock_address)
+    with pytest.raises(exceptions.VirtualMachineError):
+        partners_timelock.release({'from': owner})
+
+    chain.sleep(YEAR_IN_SECONDS + 1)
+
+    founders_timelock.release({'from': owner})
+    foundation_timelock.release({'from': owner})
+    partners_timelock.release({'from': owner})
+
+    founders_balance = dapp_token.balanceOf(founders_fund) // (10 ** decimals)
+    assert founders_balance == founders_amount
+
+    foundation_balance = dapp_token.balanceOf(foundation_fund) // (10 ** decimals)
+    assert foundation_balance == foundation_amount
+
+    partners_balance = dapp_token.balanceOf(partners_fund) // (10 ** decimals)
+    assert partners_balance == partners_amount
